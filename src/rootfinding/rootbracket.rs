18f6e1b2bfe6e3f4955b19593742f92de6988d5f
huarkiou <huarkiou@outlook.com>
2025-07-07 20:24:05 +0800

    给RootBracket添加成员函数

--- src/rootfinding/rootbracket.rs
+++ src/rootfinding/rootbracket.rs
@@ -9,6 +9,18 @@ pub struct RootBracket {
 }
 
 impl RootBracket {
+    pub fn bracket(&self) -> (f64, f64) {
+        (self.a, self.b)
+    }
+
+    pub fn root(&self) -> f64 {
+        self.a / 2. + self.b / 2.
+    }
+
+    pub fn has_root(&self) -> bool {
+        self.fa * self.fb <= 0.0
+    }
+
     pub fn is_converged(&self) -> bool {
         self.converged
     }
