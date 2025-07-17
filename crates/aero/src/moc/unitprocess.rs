636e3d6d769ac778935ab4c8075f651ef53a4cd2
huarkiou <huarkiou@outlook.com>
2025-07-17 11:47:23 +0800

    补充注释

--- crates/aero/src/moc/unitprocess.rs
+++ crates/aero/src/moc/unitprocess.rs
@@ -2,19 +2,32 @@ use crate::moc::{CharLine, MocPoint};
 
 #[derive(Clone, Copy)]
 pub struct UPContext<'a> {
+    /// 前一条特征线，不单调总体上从左上到右下
     pub prev: &'a CharLine,
+    /// 后一条特征线，不单调总体上从左上到右下
     pub next: &'a CharLine,
+    /// 前一条特征线初始点下标
     pub idx_prev: usize,
+    /// 后一条特征线初始点下标
     pub idx_next: usize,
 }
 
 #[derive(Clone, Copy)]
 pub struct GeneralConfig {
+    /// 轴对称/二维平面
     pub axisym: bool,
+    /// 容差
     pub tol: f64,
+    /// 最大预估校正次数
     pub n_corr: i64,
 }
 
+/// 出口最后一条特征线边界条件
+///
+/// # 输入参数
+/// - `y:f64`, `p:MocPoint`: 待求点y轴坐标, 参考点气流参数p
+/// # 返回值
+/// - `u:f64`, `v:f64`: 待求点气流在x和y方向的速度u, v
 pub type ExitLineFunc = Box<dyn Fn(f64, MocPoint) -> (f64, f64)>;
 
 pub trait UnitProcess {
