4970a3a199cc1d05a85187376068e71ca518e464
huarkiou <huarkiou@outlook.com>
2025-07-18 15:55:58 +0800

    暂时不用

--- crates/aero/src/moc/unitprocess/rotational.rs
+++ crates/aero/src/moc/unitprocess/rotational.rs
@@ -1,54 +1,54 @@
-use crate::moc::unitprocess::GeneralConfig;
-use crate::moc::unitprocess::UnitProcess;
+// use crate::moc::unitprocess::GeneralConfig;
+// use crate::moc::unitprocess::UnitProcess;
 
-/// 有旋特征线法的基本计算过程
-pub struct Rotational {
-    conf: GeneralConfig,
-}
+// /// 有旋特征线法的基本计算过程
+// pub struct Rotational {
+//     conf: GeneralConfig,
+// }
 
-impl UnitProcess for Rotational {
-    fn interior_point(&self, context: super::Context) -> Option<crate::moc::MocPoint> {
-        todo!()
-    }
+// impl UnitProcess for Rotational {
+//     fn interior_point(&self, context: super::Context) -> Option<crate::moc::MocPoint> {
+//         todo!()
+//     }
 
-    fn symmetry_axis_point(&self, context: super::Context) -> Option<crate::moc::MocPoint> {
-        todo!()
-    }
+//     fn symmetry_axis_point(&self, context: super::Context) -> Option<crate::moc::MocPoint> {
+//         todo!()
+//     }
 
-    fn inverse_wall_point(&self, context: super::Context) -> Option<crate::moc::MocPoint> {
-        todo!()
-    }
+//     fn inverse_wall_point(&self, context: super::Context) -> Option<crate::moc::MocPoint> {
+//         todo!()
+//     }
 
-    fn exit_characteristics_point(
-        &self,
-        context: super::Context,
-        cal_u_v: super::ExitLineFunc,
-    ) -> Option<crate::moc::MocPoint> {
-        todo!()
-    }
+//     fn exit_characteristics_point(
+//         &self,
+//         context: super::Context,
+//         cal_u_v: super::ExitLineFunc,
+//     ) -> Option<crate::moc::MocPoint> {
+//         todo!()
+//     }
 
-    fn exit_characteristics_point_fixed_dist(
-        &self,
-        context: super::Context,
-        cal_u_v: super::ExitLineFunc,
-        dist: f64,
-    ) -> Option<crate::moc::MocPoint> {
-        todo!()
-    }
+//     fn exit_characteristics_point_fixed_dist(
+//         &self,
+//         context: super::Context,
+//         cal_u_v: super::ExitLineFunc,
+//         dist: f64,
+//     ) -> Option<crate::moc::MocPoint> {
+//         todo!()
+//     }
 
-    fn transition_interior_point(&self, context: super::Context) -> Option<crate::moc::MocPoint> {
-        todo!()
-    }
+//     fn transition_interior_point(&self, context: super::Context) -> Option<crate::moc::MocPoint> {
+//         todo!()
+//     }
 
-    fn transition_wall_point(&self, context: super::Context) -> Option<crate::moc::MocPoint> {
-        todo!()
-    }
+//     fn transition_wall_point(&self, context: super::Context) -> Option<crate::moc::MocPoint> {
+//         todo!()
+//     }
 
-    fn last_point(
-        &self,
-        context: super::Context,
-        cal_u_v: super::ExitLineFunc,
-    ) -> Option<crate::moc::MocPoint> {
-        todo!()
-    }
-}
+//     fn last_point(
+//         &self,
+//         context: super::Context,
+//         cal_u_v: super::ExitLineFunc,
+//     ) -> Option<crate::moc::MocPoint> {
+//         todo!()
+//     }
+// }
