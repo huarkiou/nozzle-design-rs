88d1879d7b8401df83d42b5e0aadfe7910e56cdf
huarkiou <huarkiou@outlook.com>
2025-07-19 15:31:54 +0800

    MocPoint中物性参数改用Material实现，相比以前储存gamma和rg造成了60%性能损失，但是为了处理变比热情况还是值得的。

--- crates/aero/benches/moc_unitprocess_irrotational.rs
+++ crates/aero/benches/moc_unitprocess_irrotational.rs
@@ -1,6 +1,11 @@
-use aero::moc::{
-    CharLine, MocPoint,
-    unitprocess::{Context, GeneralConfig, Irrotational, UnitProcess},
+use std::sync::Arc;
+
+use aero::{
+    Material,
+    moc::{
+        CharLine, MocPoint,
+        unitprocess::{Context, GeneralConfig, Irrotational, UnitProcess},
+    },
 };
 use criterion::{Criterion, criterion_group, criterion_main};
 use math::Tolerance;
@@ -13,13 +18,32 @@ fn test_interior_point_1() {
     };
 
     let unitprocess = Irrotational { conf: config };
+    let mat = Arc::new(Material::from_rgas_gamma(320.0, 1.2));
+    // let mat = Arc::new(Material::new(
+    //     Material::UNIVERSAL_GAS_CONSTANT / 320.0 * 1e3,
+    //     |_| 320. * 1.2 / (1.2 - 1.0),
+    // ));
 
     // 构造两个输入点
     let p1 = MocPoint::from_compatible(
-        0.131460, 0.040118, 2473.4, 812.8, 34042.0, 3000.0, 0.086151, 1.2, 320.0,
+        0.131460,
+        0.040118,
+        2473.4,
+        812.8,
+        34042.0,
+        3000.0,
+        0.086151,
+        mat.clone(),
     );
     let p2 = MocPoint::from_compatible(
-        0.135683, 0.037123, 2502.8, 737.6, 32781.0, 3000.0, 0.083482, 1.2, 320.0,
+        0.135683,
+        0.037123,
+        2502.8,
+        737.6,
+        32781.0,
+        3000.0,
+        0.083482,
+        mat.clone(),
     );
 
     let velocity = 2628.726210082568_f64;
@@ -32,8 +56,7 @@ fn test_interior_point_1() {
         28742.423476934055,
         1200.4683626106616,
         0.07481934065705345,
-        1.2,
-        320.0,
+        mat.clone(),
     );
 
     // 创建 CharLine
@@ -71,6 +94,11 @@ fn test_interior_point_2() {
     };
 
     let unitprocess = Irrotational { conf: config };
+    let mat = Arc::new(Material::from_rgas_gamma(287.042, 1.4));
+    // let mat = Arc::new(Material::new(
+    //     Material::UNIVERSAL_GAS_CONSTANT / 287.042 * 1e3,
+    //     |_| 287.042 * 1.4 / (1.4 - 1.0),
+    // ));
 
     // 构造两个输入点
     let velocity = 1948.3337719140004_f64;
@@ -83,8 +111,7 @@ fn test_interior_point_2() {
         65.123505884851653,
         2000.0,
         0.0010063378824976170,
-        1.4,
-        287.042,
+        mat.clone(),
     );
 
     let velocity = 1955.5966975668214_f64;
@@ -97,8 +124,7 @@ fn test_interior_point_2() {
         57.598901602349798,
         2000.0,
         0.00076988609083849971,
-        1.4,
-        287.042,
+        mat.clone(),
     );
 
     let velocity = 1949.2684506799624_f64;
@@ -111,8 +137,7 @@ fn test_interior_point_2() {
         74.5290738178469,
         108.96389835620812,
         0.0010021388327088687,
-        1.4,
-        287.042,
+        mat.clone(),
     );
 
     // 创建 CharLine
--- crates/aero/src/moc/unitprocess/irrotational.rs
+++ crates/aero/src/moc/unitprocess/irrotational.rs
@@ -181,9 +181,11 @@ impl UnitProcess for Irrotational {
 
 #[cfg(test)]
 mod tests {
+    use std::sync::Arc;
+
     use math::Tolerance;
 
-    use crate::moc::CharLine;
+    use crate::{Material, moc::CharLine};
 
     use super::*;
 
@@ -196,13 +198,28 @@ mod tests {
         };
 
         let unitprocess = Irrotational { conf: config };
+        let mat = Arc::new(Material::from_rgas_gamma(320.0, 1.2));
 
         // 构造两个输入点
         let p1 = MocPoint::from_compatible(
-            0.131460, 0.040118, 2473.4, 812.8, 34042.0, 3000.0, 0.086151, 1.2, 320.0,
+            0.131460,
+            0.040118,
+            2473.4,
+            812.8,
+            34042.0,
+            3000.0,
+            0.086151,
+            mat.clone(),
         );
         let p2 = MocPoint::from_compatible(
-            0.135683, 0.037123, 2502.8, 737.6, 32781.0, 3000.0, 0.083482, 1.2, 320.0,
+            0.135683,
+            0.037123,
+            2502.8,
+            737.6,
+            32781.0,
+            3000.0,
+            0.083482,
+            mat.clone(),
         );
 
         let velocity = 2628.726210082568_f64;
@@ -215,8 +232,7 @@ mod tests {
             28742.423476934055,
             1200.4683626106616,
             0.07481934065705345,
-            1.2,
-            320.0,
+            mat.clone(),
         );
 
         // 创建 CharLine
@@ -255,6 +271,7 @@ mod tests {
         };
 
         let unitprocess = Irrotational { conf: config };
+        let mat = Arc::new(Material::from_rgas_gamma(287.042, 1.4));
 
         // 构造两个输入点
         let velocity = 1948.3337719140004_f64;
@@ -267,8 +284,7 @@ mod tests {
             65.123505884851653,
             2000.0,
             0.0010063378824976170,
-            1.4,
-            287.042,
+            mat.clone(),
         );
 
         let velocity = 1955.5966975668214_f64;
@@ -281,8 +297,7 @@ mod tests {
             57.598901602349798,
             2000.0,
             0.00076988609083849971,
-            1.4,
-            287.042,
+            mat.clone(),
         );
 
         let velocity = 1949.2684506799624_f64;
@@ -295,8 +310,7 @@ mod tests {
             74.5290738178469,
             108.96389835620812,
             0.0010021388327088687,
-            1.4,
-            287.042,
+            mat.clone(),
         );
 
         // 创建 CharLine
