9b1d06c612139b248dd01d60dea584dfa8e0c992
huarkiou <huarkiou@outlook.com>
2025-11-15 19:17:12 +0800

    完善bench

--- crates/aero/benches/moc_unitprocess_irrotational.rs
+++ crates/aero/benches/moc_unitprocess_irrotational.rs
@@ -18,10 +18,6 @@ fn test_interior_point_1() {
 
     let unitprocess = Irrotational { conf: config };
     let mat = Material::from_rgas_gamma(320.0, 1.2);
-    // let mat = Material::new(
-    //     Material::UNIVERSAL_GAS_CONSTANT / 320.0 * 1e3,
-    //     |_| 320. * 1.2 / (1.2 - 1.0),
-    // );
 
     // 构造两个输入点
     let p1 = MocPoint::from_compatible(
@@ -45,8 +41,7 @@ fn test_interior_point_1() {
         mat.clone(),
     );
 
-    let velocity = 2628.726210082568_f64;
-    let theta = 0.3013949963150419_f64;
+    let (velocity, theta): (f64, f64) = (2628.726210082568, 0.3013949963150419);
     let target = MocPoint::new(
         0.14113488139562955,
         0.040536826493196544,
@@ -94,14 +89,9 @@ fn test_interior_point_2() {
 
     let unitprocess = Irrotational { conf: config };
     let mat = Material::from_rgas_gamma(287.042, 1.4);
-    // let mat = Material::new(
-    //     Material::UNIVERSAL_GAS_CONSTANT / 287.042 * 1e3,
-    //     |_| 287.042 * 1.4 / (1.4 - 1.0),
-    // );
 
     // 构造两个输入点
-    let velocity = 1948.3337719140004_f64;
-    let theta = 0.43988113776612270_f64;
+    let (velocity, theta): (f64, f64) = (1948.3337719140004, 0.43988113776612270);
     let p1 = MocPoint::from_compatible(
         5.9734147955750752,
         1.1257175564978437,
@@ -113,8 +103,7 @@ fn test_interior_point_2() {
         mat.clone(),
     );
 
-    let velocity = 1955.5966975668214_f64;
-    let theta = 0.47112060764605074_f64;
+    let (velocity, theta): (f64, f64) = (1955.5966975668214, 0.47112060764605074);
     let p2 = MocPoint::from_compatible(
         6.0,
         1.1247947107403191,
@@ -126,8 +115,7 @@ fn test_interior_point_2() {
         mat.clone(),
     );
 
-    let velocity = 1949.2684506799624_f64;
-    let theta = 0.43805199441312853_f64;
+    let (velocity, theta): (f64, f64) = (1949.2684506799624, 0.43805199441312853);
     let target = MocPoint::new(
         6.036241021295845,
         1.1473941269582562,
@@ -166,13 +154,172 @@ fn test_interior_point_2() {
     );
 }
 
+#[warn(unused)]
+fn test_interior_point_1_variable() {
+    let config = GeneralConfig {
+        axisym: AreaType::Axisymmetric,
+        tol: Tolerance::new(1e-5, 1e-5),
+        n_corr: 20,
+    };
+
+    let unitprocess = Irrotational { conf: config };
+    let mat = Material::air_nasa9piecewise_polynomial();
+
+    // 构造两个输入点
+    let p1 = MocPoint::from_compatible(
+        0.131460,
+        0.040118,
+        2473.4,
+        812.8,
+        34042.0,
+        3000.0,
+        0.086151,
+        mat.clone(),
+    );
+    let p2 = MocPoint::from_compatible(
+        0.135683,
+        0.037123,
+        2502.8,
+        737.6,
+        32781.0,
+        3000.0,
+        0.083482,
+        mat.clone(),
+    );
+
+    let (velocity, theta): (f64, f64) = (2628.726210082568, 0.3013949963150419);
+    let target = MocPoint::new(
+        0.14113488139562955,
+        0.040536826493196544,
+        velocity * theta.cos(),
+        velocity * theta.sin(),
+        28742.423476934055,
+        1200.4683626106616,
+        0.07481934065705345,
+        mat.clone(),
+    );
+
+    // 创建 CharLine
+    let mut next_line = CharLine::new();
+    next_line.push(p1.clone());
+
+    let mut prev_line = CharLine::new();
+    prev_line.push(p2.clone());
+
+    let context = Context {
+        prev: &prev_line,
+        next: &next_line,
+        idx_prev: 0,
+        idx_next: 0,
+    };
+
+    let result_point = unitprocess
+        .interior_point(context)
+        .expect("interior point should be Some");
+
+    assert!(
+        result_point.is_converged_with(&target, unitprocess.conf.tol),
+        "Interior point did not converge to expected value:\nresult:{:15}\ntarget:{:15}\n  diff:{}",
+        result_point,
+        target,
+        &result_point - &target
+    );
+}
+
+fn test_interior_point_2_variable() {
+    let config = GeneralConfig {
+        axisym: AreaType::Axisymmetric,
+        tol: Tolerance::new(1e-5, 1e-5),
+        n_corr: 20,
+    };
+
+    let unitprocess = Irrotational { conf: config };
+    let mat = Material::air_nasa9piecewise_polynomial();
+
+    // 构造两个输入点
+    let (velocity, theta): (f64, f64) = (1948.3337719140004, 0.43988113776612270);
+    let p1 = MocPoint::from_compatible(
+        5.9734147955750752,
+        1.1257175564978437,
+        velocity * theta.cos(),
+        velocity * theta.sin(),
+        65.123505884851653,
+        2000.0,
+        0.0010063378824976170,
+        mat.clone(),
+    );
+
+    let (velocity, theta): (f64, f64) = (1955.5966975668214, 0.47112060764605074);
+    let p2 = MocPoint::from_compatible(
+        6.0,
+        1.1247947107403191,
+        velocity * theta.cos(),
+        velocity * theta.sin(),
+        57.598901602349798,
+        2000.0,
+        0.00076988609083849971,
+        mat.clone(),
+    );
+
+    let (velocity, theta): (f64, f64) = (1949.32887801886, 0.4376492892161206);
+    let target = MocPoint::new(
+        6.0371989757537925,
+        1.147865098405942,
+        velocity * theta.cos(),
+        velocity * theta.sin(),
+        74.99000723946135,
+        104.47335159561544,
+        0.001005369819472018,
+        mat.clone(),
+    );
+
+    // 创建 CharLine
+    let mut next_line = CharLine::new();
+    next_line.push(p1.clone());
+
+    let mut prev_line = CharLine::new();
+    prev_line.push(p2.clone());
+
+    let context = Context {
+        prev: &prev_line,
+        next: &next_line,
+        idx_prev: 0,
+        idx_next: 0,
+    };
+
+    let result_point = unitprocess
+        .interior_point(context)
+        .expect("interior point should be Some");
+
+    assert!(
+        result_point.is_converged_with(&target, unitprocess.conf.tol),
+        "Interior point did not converge to expected value:\nresult:{:15}\ntarget:{:15}\n  diff:{}",
+        result_point,
+        target,
+        &result_point - &target
+    );
+}
+
 fn bench_interior_point(c: &mut Criterion) {
-    c.bench_function("moc::unitprocess::irrotational::interior_point()", |b| {
-        b.iter(|| {
-            test_interior_point_1();
-            test_interior_point_2();
-        })
-    });
+    c.bench_function(
+        "moc::unitprocess::irrotational::interior_point() constant cp",
+        |b| {
+            b.iter(|| {
+                test_interior_point_1();
+                test_interior_point_2();
+            })
+        },
+    );
+
+    c.bench_function(
+        "moc::unitprocess::irrotational::interior_point() variable cp",
+        |b| {
+            b.iter(|| {
+                // test_interior_point_1_variable();
+                test_interior_point_2_variable();
+            })
+        },
+    );
 }
 
 criterion_group!(benches, bench_interior_point);
